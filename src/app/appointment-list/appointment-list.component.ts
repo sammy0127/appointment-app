bfcb6a55caba39e4ecda31407e2238e546683753
Sam Eads <sam.eads@gmail.com>
2024-09-11 00:18:12 -0400

    Before 2 way data binding

---
+++ src/app/appointment-list/appointment-list.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Appointment } from '../models/appointment'
+
+@Component({
+  selector: 'app-appointment-list',
+  templateUrl: './appointment-list.component.html',
+  styleUrls: ['./appointment-list.component.css']
+})
+export class AppointmentListComponent {
+
+  appointment: Appointment = {
+    id: 1,
+    title: "Take dog for a walk",
+    date: new Date('2024-09-13')
+  }
+
+}
